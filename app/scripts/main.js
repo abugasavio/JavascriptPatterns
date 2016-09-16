710943fbc723b48a81ae6f960ba9706fe80dffff
savioabuga <savioabuga@gmail.com>
2016-09-16 22:20:42 +0300

    loading images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+(function(_, $){
+  Cat.loadAll();
+
+  var $gallery = $('[rel=gallery]');
+
+
+  var keys = Object.keys(Cat.instances);
+
+
+  var compiled = _.template('<div class="col-lg-4 col-sm-6 col-xs-12">\
+    <a href="#">\
+    <img src="<%= url%>" class="thumbnail img-responsive">\
+    </a>\
+    </div>');
+
+
+  for(var i=0; i < keys.length; i++) {
+    $gallery.append(compiled(Cat.instances[keys[i]]));
+  }
+})(_, $);
---
+++ app/scripts/model.js
@@ -0,0 +1,49 @@
+function Cat(slots){
+  this.url = slots.url;
+  this.name = slots.name;
+}
+
+Cat.instances = {};
+
+Cat.updateInstances = function(json){
+  var key = "";
+  var keys = Object.keys(json);
+  for (var i = 0; i < keys.length; i++) {
+    key = keys[i];
+    var c = new Cat(json[key]);
+    console.log(c);
+    Cat.instances[key] = c;
+  }
+}
+
+Cat.loadAll2 = function(){
+  fetch('/data/data.json')
+    .then(function(response){
+      return response.json();
+    }).then(function(json){
+      console.log(json);
+      Cat.updateInstances([
+        {
+          "name":"cat01",
+          "url":"images/cat01.jpg"
+        },
+        {
+          "name":"cat02",
+          "url":"images/cat02.jpg"
+        }
+      ]);
+  });
+};
+
+Cat.loadAll = function(){
+  Cat.updateInstances([
+    {
+      "name":"cat01",
+      "url":"images/cat01.jpg"
+    },
+    {
+      "name":"cat02",
+      "url":"images/cat02.jpg"
+    }
+  ])
+};
