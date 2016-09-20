24488d5e8db91312ed40e3f48dfbe1727202f7cc
savioabuga <savioabuga@gmail.com>
2016-09-20 15:25:42 +0300

    aesthetics ...

--- app/scripts/catclicker2/catclicker2.js
+++ app/scripts/catclicker2/catclicker2.js
@@ -1,40 +1,45 @@
 (function($, _){
   var model = {
+
     init: function(){
       if(!localStorage.cats) {
         localStorage.cats = JSON.stringify([
           {
-            "name":"cat01",
-            "url":"images/cat01.jpg"
+            'name':'cat01',
+            'url':'images/cat01.jpg'
           },
           {
-            "name":"cat02",
-            "url":"images/cat02.jpg"
+            'name':'cat02',
+            'url':'images/cat02.jpg'
           }
         ]);
       }
     },
+
     getAllCats: function(){
       return JSON.parse(localStorage.cats);
     },
+
     getCat: function(catName){
       return model.getAllCats().filter(function(catObj){
         return catObj.name === catName;
       })
-
     },
 
   };
 
   var octopus = {
+
     init: function(){
       model.init();
       console.log(model.getAllCats());
       view.init();
     },
+
     getCats: function () {
       return model.getAllCats();
     },
+
     catClicked: function(evt){
       var catName = $(evt.target).html();
       view.updateCatThumbnail(model.getCat(catName)[0]);
@@ -43,22 +48,24 @@
   };
 
   var view = {
+
     init: function() {
       view.buildList();
     },
+
     buildList: function(){
       var $catlist = $('#catlist');
       var catlistTemplate = _.template('<li ref="catlistLi" class="list-group-item" name=<%= cat.name%>><a href="#"><%= cat.name%></a></li>');
       var cats = octopus.getCats();
-      for(var i =0; i < cats.length; i++) {
+      for(var i = 0; i < cats.length; i++) {
         $catlist.append(catlistTemplate({cat: cats[i]}));
       }
       var $catListLinks = $('[ref=catlistLi] > a');
       $catListLinks.on('click', octopus.catClicked);
     },
+
     updateCatThumbnail: function(catObj){
       var $thumbnail = $('#catThumbnail');
-
       var catThumbnailTemplate=_.template('<div class="thumbnail">\
         <img class="img-rounded no-resize" rel="catimage" id="catimage" data-src="holder.js/100%x200" alt="..." src=<%= cat.url %>>\
         <div class="caption">\
@@ -70,9 +77,7 @@
 
       $thumbnail.html('');
       $thumbnail.append(catThumbnailTemplate({cat:catObj}))
-
     }
-
   };
 
   octopus.init();
